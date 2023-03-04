d8fdcffa762b670786d1e6ffb6dabfb0750258ad
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-03-04 11:24:02 +0100

    Remove wrong var setter

--- addons/QuickSceneRunner/QuickSceneRunner.gd
+++ addons/QuickSceneRunner/QuickSceneRunner.gd
@@ -10,8 +10,6 @@ func _enter_tree():
 	add_control_to_bottom_panel(dock, "Quick Scenes")
 	button = Button.new()
 	button.pressed.connect(dock.run_scene)
-	button.dock = dock
-	button.plugin = self
 	button.icon = get_editor_interface().get_base_control().get_theme_icon("TransitionSync", "EditorIcons")
 	add_control_to_container(CONTAINER_TOOLBAR, button)
 	button.get_parent().move_child(button, button.get_index() - 2)
